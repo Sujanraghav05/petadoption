b508781dcca6949e09e5dcebc79ee1775cca3b8c
Sujan Raghav <sujanraghav2005@gmail.com>
2026-01-14 19:13:36 +0530

    Update App.js

--- App.js
+++ App.js
@@ -7,10 +7,15 @@ import Adoption from "./Adoption";
 import Contact from "./Contact";
 import ThankYou from "./Thankyou";
 import Status from "./Status";
+import Footer from "./footer";
 
 import AdminLogin from "./AdminLogin";
 import AdminRegister from "./AdminRegister";
 import AdminDashboard from "./AdminDashboard";
+
+import UserLogin from "./UserLogin";
+import UserRegister from "./UserRegister";
+import UserDashboard from "./UserDashboard";
 
 import Navbar from "./Navbar";
 import "./App.css";
@@ -19,6 +24,12 @@ import "./App.css";
 const AdminRoute = ({ children }) => {
     const token = localStorage.getItem("adminToken");
     return token ? children : <Navigate to="/admin/login" replace />;
+};
+
+/* Simple User Protection */
+const UserRoute = ({ children }) => {
+    const token = localStorage.getItem("userToken");
+    return token ? children : <Navigate to="/user/login" replace />;
 };
 
 function App() {
@@ -43,6 +54,10 @@ function App() {
                 <Route path="/admin/login" element={<AdminLogin />} />
                 <Route path="/admin/register" element={<AdminRegister />} />
 
+                {/* User Auth */}
+                <Route path="/user/login" element={<UserLogin />} />
+                <Route path="/user/register" element={<UserRegister />} />
+
                 {/* Admin Dashboard (Protected) */}
                 <Route
                     path="/admin"
@@ -53,9 +68,30 @@ function App() {
                     }
                 />
 
+                {/* User Dashboard (Protected) */}
+                <Route
+                    path="/user/dashboard"
+                    element={
+                        <UserRoute>
+                            <UserDashboard />
+                        </UserRoute>
+                    }
+                />
+
+                {/* User Profile (Protected) */}
+                <Route
+                    path="/user/edit-profile"
+                    element={
+                        <UserRoute>
+                            <UserDashboard /> {/* You can create a separate EditProfile component */}
+                        </UserRoute>
+                    }
+                />
+
                 {/* Fallback */}
                 <Route path="*" element={<Navigate to="/" replace />} />
             </Routes>
+            <Footer />
         </>
     );
 }
